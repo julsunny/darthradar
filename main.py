bf99e620ed3f76aca028f27e7498c4c23b5f679d
Massin Guerdi <massinguerdi@web.de>
2021-04-24 21:52:43 +0200

    fixes to main

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from dataloader import RadarImageTargetSet
 import peakDetection
 import clustering_lib
 import detection
+import numpy as np
 
-if __name__ == "__main__":
+if True or __name__ == "__main__":
     # create dataset
     ds = RadarImageTargetSet()
 
@@ -13,11 +14,11 @@ if __name__ == "__main__":
         predicted_number_of_peaks, peaks_xs, peaks_ys, peak_strengths = peakDetection.detect_peaks(img, tx=10, ty=3, gx=2, gy=1, rate_fa=1e-2)
 
         # build large boxes around peaks
-        predicted_boxes = peakDetection.return_box_bounds(peaks_x, peaks_y, 11, 27)
-        assert predicted_number_of_peaks == len(boxes)
+        predicted_boxes = peakDetection.return_box_bounds(peaks_xs, peaks_ys, img, 11, 27)
+        assert predicted_number_of_peaks == len(predicted_boxes)
         
         # refine boxes using spectral graph clustering
-        refined_boxes = np.array([clustering.refine_box(img, box, strength, n_objects = 1) for box, strength in zip(predicted_boxes, peak_strengths)])
+        refined_boxes = np.array([clustering_lib.refine_box(img, box, strength, n_objects = 1, color_scaling = 1) for box, strength in zip(predicted_boxes, peak_strengths)])
 
         # add refined boxes to dataset
         target['refined_boxes'] = refined_boxes
